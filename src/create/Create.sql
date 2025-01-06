5d0130334a9d3f483c9c13a7b1aa9161fc1936b9
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-06 19:58:18 -0300

    style: format sql

--- src/create/Create.sql
+++ src/create/Create.sql
@@ -1,89 +1,96 @@
-CREATE TABLE curso (
-    id bigint PRIMARY KEY,
-    nome varchar(30) UNIQUE NOT NULL, 
-    id_coordenador BIGINT NOT NULL,
-    regime varchar(9) NOT NULL, 
-    duracao integer NOT NULL
-); 
+CREATE TABLE curso
+  (
+     id             BIGINT PRIMARY KEY,
+     nome           VARCHAR(30) UNIQUE NOT NULL,
+     id_coordenador BIGINT NOT NULL,
+     regime         VARCHAR(9) NOT NULL,
+     duracao        INTEGER NOT NULL
+  );
 
-CREATE TABLE professor (
-	id bigint PRIMARY KEY, 
-	nome varchar(60) NOT NULL, 
-	area_professor varchar(60) NOT NULL, 
-	id_curso bigint, 
-	contato varchar(60), 
-	FOREIGN KEY (id_curso) REFERENCES curso(id)
-); 
+CREATE TABLE professor
+  (
+     id             BIGINT PRIMARY KEY,
+     nome           VARCHAR(60) NOT NULL,
+     area_professor VARCHAR(60) NOT NULL,
+     id_curso       BIGINT,
+     contato        VARCHAR(60),
+     FOREIGN KEY (id_curso) REFERENCES curso (id)
+  );
 
-ALTER TABLE curso 
-ADD CONSTRAINT fk_id_coordenador FOREIGN KEY (id_coordenador) REFERENCES professor(id); 
+ALTER TABLE curso
+  ADD CONSTRAINT fk_id_coordenador FOREIGN KEY (id_coordenador) REFERENCES
+  professor (id);
 
-CREATE TABLE turma (
-	id bigint PRIMARY KEY, 
-	nome varchar(60) NOT NULL, 
-	id_curso bigint NOT NULL, 
-	semestre integer NOT NULL,
-	cap_maxima integer NOT NULL, 
-	estado varchar(15) NOT NULL, 
-	FOREIGN KEY (id_curso) REFERENCES curso(id)
-);
+CREATE TABLE turma
+  (
+     id         BIGINT PRIMARY KEY,
+     nome       VARCHAR(60) NOT NULL,
+     id_curso   BIGINT NOT NULL,
+     semestre   INTEGER NOT NULL,
+     cap_maxima INTEGER NOT NULL,
+     estado     VARCHAR(15) NOT NULL,
+     FOREIGN KEY (id_curso) REFERENCES curso (id)
+  );
 
-CREATE TABLE aluno (
-	id bigint PRIMARY KEY, 
-	matricula bigint UNIQUE NOT NULL,
-	nome varchar(60) NOT NULL, 
-	id_curso bigint NOT NULL,
-	idade integer NOT NULL, 
-	nascimento date NOT NULL, 
-	entrada integer NOT NULL, 
-	conclusao integer NOT NULL, 
-	CHECK (entrada <= conclusao),
-	FOREIGN KEY (id_curso) REFERENCES curso(id)
-); 
+CREATE TABLE aluno
+  (
+     id         BIGINT PRIMARY KEY,
+     matricula  BIGINT UNIQUE NOT NULL,
+     nome       VARCHAR(60) NOT NULL,
+     id_curso   BIGINT NOT NULL,
+     idade      INTEGER NOT NULL,
+     nascimento DATE NOT NULL,
+     entrada    INTEGER NOT NULL,
+     conclusao  INTEGER NOT NULL,
+     CHECK (entrada <= conclusao),
+     FOREIGN KEY (id_curso) REFERENCES curso (id)
+  );
 
-CREATE TABLE avaliacao (
-	id bigint PRIMARY KEY, 
-	tipo varchar(20) NOT NULL, 
-	data_aplicacao timestamp NOT NULL, 
-	peso integer NOT NULL
-); 
+CREATE TABLE avaliacao
+  (
+     id             BIGINT PRIMARY KEY,
+     tipo           VARCHAR(20) NOT NULL,
+     data_aplicacao TIMESTAMP NOT NULL,
+     peso           INTEGER NOT NULL
+  );
 
-CREATE TABLE nota (
-	id bigint PRIMARY KEY, 
-	id_avaliacao bigint NOT NULL, 
-	valor real NOT NULL CHECK (valor >= 0 AND valor <= 10), 
-	id_aluno bigint NOT NULL, 
-	FOREIGN KEY(id_avaliacao) REFERENCES avaliacao(id), 
-	FOREIGN KEY(id_aluno) REFERENCES aluno(id) 
-); 
+CREATE TABLE nota
+  (
+     id           BIGINT PRIMARY KEY,
+     id_avaliacao BIGINT NOT NULL,
+     valor        REAL NOT NULL CHECK ( valor >= 0 AND valor <= 10 ),
+     id_aluno     BIGINT NOT NULL,
+     FOREIGN KEY (id_avaliacao) REFERENCES avaliacao (id),
+     FOREIGN KEY (id_aluno) REFERENCES aluno (id)
+  );
 
-CREATE TABLE sala_de_aula (
-	id bigint PRIMARY KEY, 
-	tipo_estrutura smallint NOT NULL, 
-	tipo_lousa smallint NOT NULL, 
-	capacidade integer NOT NULL, 
-	localizacao varchar(255) NOT NULL 
-); 
+CREATE TABLE sala_de_aula
+  (
+     id             BIGINT PRIMARY KEY,
+     tipo_estrutura SMALLINT NOT NULL,
+     tipo_lousa     SMALLINT NOT NULL,
+     capacidade     INTEGER NOT NULL,
+     localizacao    VARCHAR(255) NOT NULL
+  );
 
-CREATE TABLE material_didatico (
-	id bigint PRIMARY KEY, 
-	titulo varchar(40) NOT NULL, 
-	descricao varchar(255) NOT NULL, 
-	formato smallint NOT NULL
-); 
+CREATE TABLE material_didatico
+  (
+     id        BIGINT PRIMARY KEY,
+     titulo    VARCHAR(40) NOT NULL,
+     descricao VARCHAR(255) NOT NULL,
+     formato   SMALLINT NOT NULL
+  );
 
-CREATE TABLE projeto_pesquisa (
-	id bigint PRIMARY KEY, 
-	titulo varchar(40) NOT NULL,
-	id_coordenador bigint NOT NULL, 
-	area varchar(60) NOT NULL, 
-	descricao varchar(255) NOT NULL, 
-	orc_planejado real NOT NULL, 
-	orc_realizado real, 
-	estado smallint NOT NULL, 
-	FOREIGN KEY (id_coordenador) REFERENCES professor(id) 
-); 
-
--- Relations 
-
-
+CREATE TABLE projeto_pesquisa
+  (
+     id             BIGINT PRIMARY KEY,
+     titulo         VARCHAR(40) NOT NULL,
+     id_coordenador BIGINT NOT NULL,
+     area           VARCHAR(60) NOT NULL,
+     descricao      VARCHAR(255) NOT NULL,
+     orc_planejado  REAL NOT NULL,
+     orc_realizado  REAL,
+     estado         SMALLINT NOT NULL,
+     FOREIGN KEY (id_coordenador) REFERENCES professor (id)
+  );
+-- Relations
