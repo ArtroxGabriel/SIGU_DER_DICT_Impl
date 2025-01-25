3c7112631af6b3b3b33de0f94b43872e2248fc50
ArtroxGabriel <artroxgabriel@gmail.com>
2025-01-25 16:47:18 -0300

    feat: create tables query

--- trabalho2/cli/create-tables.py
+++ trabalho2/cli/create-tables.py
@@ -1,9 +1,58 @@
 import psycopg
 
-USER_NAME = "539628"
-USER_PWD = "539628"
-DB_HOST = "200.129.44.249"
-DB_NAME = "antgabriel_539628_ed1"
+USER_NAME = "postgres"
+USER_PWD = "postgres"
+DB_HOST = "localhost"  # "200.129.44.249"
+DB_NAME = "teste_db"
+
+CREATE_QUERY = """
+CREATE TABLE IF NOT EXISTS Curso (
+	id INTEGER PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL CHECK (nome <> ''),
+	regime VARCHAR(20) NOT NULL CHECK (regime <> ''),
+	duracao INT NOT NULL CHECK (duracao > 0)
+);
+
+CREATE TABLE IF NOT EXISTS Aluno (
+	id INT PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL CHECK (nome <> ''),
+	curso_id INT NOT NULL REFERENCES Curso (id),
+	semestre INT NOT NULL CHECK (semestre > 0)
+);
+
+CREATE TABLE IF NOT EXISTS Professor (
+	id INT PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL CHECK (nome <> ''),
+	area_especializacao VARCHAR(100) NOT NULL CHECK (area_especializacao <> ''),
+	contato VARCHAR(100) NOT NULL CHECK (contato <> ''),
+	curso_id INT NOT NULL REFERENCES Curso (id)
+);
+
+CREATE TABLE IF NOT EXISTS Disciplina (
+	id INT PRIMARY KEY,
+	codigo VARCHAR(10) UNIQUE NOT NULL CHECK (codigo <> ''),
+	nome VARCHAR(100) NOT NULL CHECK (nome <> ''),
+	area_especializacao VARCHAR(100) NOT NULL CHECK (area_especializacao <> ''),
+	carga_horaria INT NOT NULL CHECK (carga_horaria > 0),
+	curso_id INT NOT NULL REFERENCES Curso (id)
+);
+
+CREATE TABLE IF NOT EXISTS Turma (
+	id INT PRIMARY KEY,
+	codigo VARCHAR(10) UNIQUE NOT NULL CHECK (codigo <> ''),
+	disciplina_id INT NOT NULL REFERENCES Disciplina (id),
+	semestre INT NOT NULL CHECK (semestre > 0),
+	capacidade_maxima INT NOT NULL CHECK (capacidade_maxima > 0),
+	estado VARCHAR(20) NOT NULL CHECK (estado <> ''),
+	prof_id INT REFERENCES Professor (id)
+);
+
+CREATE TABLE IF NOT EXISTS Aluno_Turma (
+	aluno_id INT NOT NULL REFERENCES Aluno (id),
+	turma_id INT NOT NULL REFERENCES Turma (id),
+	PRIMARY KEY (aluno_id, turma_id)
+);
+"""
 
 
 def create_tables() -> None:
@@ -12,31 +61,8 @@ def create_tables() -> None:
     ) as conn:
         # Open a cursor to perform database operations
         with conn.cursor() as cur:
-            # Execute a command: this creates a new table
-            cur.execute("""
-                CREATE TABLE IF NOT EXISTS test (
-                    id serial PRIMARY KEY,
-                    num integer,
-                    data text)
-                """)
+            cur.execute(CREATE_QUERY)
 
-            # Pass data to fill a query placeholders and let Psycopg perform
-            # the correct conversion (no SQL injections!)
-            cur.execute(
-                "INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def")
-            )
-
-            # Query the database and obtain data as Python objects.
-            cur.execute("SELECT * FROM test")
-            cur.fetchone()
-            # will return (1, 100, "abc'def")
-
-            # You can use `cur.fetchmany()`, `cur.fetchall()` to return a list
-            # of several records, or even iterate on the cursor
-            for record in cur:
-                print(record)
-
-            # Make the changes to the database persistent
             conn.commit()
 
 
